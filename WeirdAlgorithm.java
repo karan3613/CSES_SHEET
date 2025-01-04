c9f3da6920b63386840f938e56baea7cbb835970
karan3613 <karanchauhan.3613@gmail.com>
2025-01-05 01:04:01 +0530

    WeirdAlgorithm

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -6,14 +6,14 @@ class WeirdAlgorithm {
        Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
        print(n) ;
+       sc.close();
     }
-    public static void print(int n){
+    public static void print(long n){
        if(n==1){
         System.out.print(n);
         return ;
        } 
-       System.out.print(n);
-       System.out.print("->"); 
+       System.out.print(n+" "); 
        if(n%2==0)print(n/2);
        else print(n*3 + 1);
     }
