ff625c42db64c7f53af0724e4f4a30e24a98a490
karan3613 <karanchauhan.3613@gmail.com>
2025-01-06 00:07:45 +0530

    Permutations.java

--- IncreasingArray.java
+++ IncreasingArray.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class IncreasingArray {
+import java.util.* ;
+
+public class IncreasingArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+        long[] arr = new long[n];
+        for(int i =0 ; i<n ; i++){
+           arr[i] = sc.nextInt() ;
+        }
+        System.out.println(helper(arr));
+    }
+    public  static long helper(long[] arr){
+        long count = 0 ;
+        for(int i =1 ; i<arr.length ; i++){
+            if(arr[i]>=arr[i-1]) continue;
+            else {
+                count+=(arr[i-1]-arr[i]);
+                arr[i] = arr[i-1];
+            }
+        }
+        return count  ;
+    }
 }
---
+++ NumberSpiral.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NumberSpiral {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+        for(int i = 0 ; i<n ; i++){
+            int y = sc.nextInt() ;
+            int x = sc.nextInt() ; 
+        }
+        helper(n);
+        sc.close();
+    }
+    public static  int helper(int n){
+
+    }
+}
